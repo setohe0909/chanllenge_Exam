1b6ebdbb398fcf4f6b6a06a3912dba57bead6bdc
setohe0909 <setohe.09@gmail.com>
2015-07-30 18:38:27 -0500

    Creacion de matriz 3d

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,6 @@
  */
 angular.module('challengeApp')
   .controller('MainCtrl', function ($scope) {
-  	var index = parseInt($scope.cordenada);
-	var value = parseInt($scope.value);
-	var updateArray = $scope.arr;
   	$scope.newMatriz = function(){
 		$scope.newArray =
 			$scope.createMatriz(parseInt($scope.dimension), parseInt($scope.dimension));
@@ -27,20 +24,4 @@ angular.module('challengeApp')
 	    } else return 0;
     	return $scope.arr = matriz;
     };
-    $scope.updateMatriz = function(){
-    	for (var iMat = 0; updateArray.length > iMat; iMat++)
-		    updateArray[iMat][index] += value;
-		return $scope.arr = updateArray;
-    };
-    $scope.sumDatos = function(){
-    	var sum = 0;
-        var init = parseInt($scope.init);
-    	var end = parseInt($scope.end);
-    	var updateArray = $scope.arr;
-
-    	for (var iMat = 0; updateArray.length > iMat; iMat++)
-		    sum += updateArray[iMat][index];
-
-		return $scope.sumatoria = sum;
-    }
   });
